53b8963775557119667ccee0f368d3681335ba2d
Erick272548 <erickismael548@gmail.com>
2025-11-19 08:52:43 -0500

    f

--- conexion.php
+++ conexion.php
@@ -4,7 +4,7 @@ $user = 'root';
 $pass = '';
 $db = 'proyecto_web'; 
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, '3350');
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
